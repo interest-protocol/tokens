70632ebabeb6e9a6f71065634491803a04d1cdce
git-marcopitra <marcomorete73@hotmail.com>
2024-09-17 17:25:33 +0100

    :arrow_up: feat: upgrade sui tokens pkg

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,6 @@ export const STRICT_LIST: ReadonlyArray<Token> = [
   },
   {
     decimals: 6,
-    name: 'USDC',
-    symbol: 'USDC',
-    type: '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC',
-    kind: 'StableCoin',
-  },
-  {
-    decimals: 6,
     chain: 'ETH',
     name: 'wUSDCe',
     symbol: 'USDC',
