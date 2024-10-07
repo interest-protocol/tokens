29d1a3fe6d169c436dfae53793c3579b42e8eac3
KipandaJr <antoniokipandacardoso@gmail.com>
2024-10-07 23:29:04 +0100

    :sparkles: feat: revert USDC position

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,19 @@ export const STRICT_LIST: ReadonlyArray<Token> = [
   },
   {
     decimals: 6,
+    name: 'USDC',
+    symbol: 'USDC',
+    type: '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC',
+    kind: 'StableCoin',
+  },
+  {
+    decimals: 6,
     chain: 'ETH',
     name: 'wUSDCe',
     symbol: 'USDC',
     bridge: 'wormhole',
     type: '0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN',
     logoUrl: 'https://strapi-dev.scand.app/uploads/usdc_8cc5687a10.png',
-    kind: 'StableCoin',
-  },
-  {
-    decimals: 6,
-    name: 'USDC',
-    symbol: 'USDC',
-    type: '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC',
     kind: 'StableCoin',
   },
   {
