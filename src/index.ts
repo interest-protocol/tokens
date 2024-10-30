d8f31ad1ef238b1282c76ac8338f792a283976c9
git-marcopitra <marcomorete73@hotmail.com>
2024-10-30 01:38:00 +0100

    :bookmark: feat: major latest

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Token } from './types';
 import stringListJson from '../sui.json';
 
-export const STRICT_LIST = stringListJson as ReadonlyArray<Token>;
+export const SUI_VERIFIED_COINS = stringListJson as ReadonlyArray<Token>;
+
+export const getSuiVerifiedCoins = (): Promise<ReadonlyArray<Token>> =>
+  fetch('https://interest-protocol.github.io/tokens/sui.json').then((res) =>
+    res.json?.()
+  );
 
 export * from './types';
